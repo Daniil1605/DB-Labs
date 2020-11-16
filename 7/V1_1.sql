fdbec203657d250ea6ed1ab265373814399d0589
Daniil1605 <48051080+Daniil1605@users.noreply.github.com>
2020-11-16 23:39:41 +0300

    Add files via upload

--- 7/V1_1.sql
+++ 7/V1_1.sql
@@ -1,32 +1,47 @@
 use AdventureWorks2012;
 go
 
-/*—оздайте хранимую процедуру, котора¤ будет возвращать сводную таблицу (оператор
-PIVOT), отображающую данные о суммарном количестве проданных продуктов
-(Sales.SalesOrderDetail.OrderQty) за определенный год (Sales.SalesOrderHeader.OrderDate).
-—писок лет передайте в процедуру через входной параметр.
+/*¬ывести значени¤ полей [BusinessEntityID], [NationalIDNumber] и [JobTitle] из таблицы
+[HumanResources].[Employee] в виде xml, сохраненного в переменную. ‘ормат xml
+должен соответствовать примеру:
 */
+select
+        HumanResources.Employee.BusinessEntityID '@ID',
+        HumanResources.Employee.NationalIDNumber as 'NationalIDNumber',
+        HumanResources.Employee.JobTitle as 'JobTitle'
+    from
+        HumanResources.Employee
+    for xml
+        path ('Employee'),
+        root ('Employees')
+/*
+—оздать временную таблицу и заполнить еЄ данными из переменной, содержащей xml.
+*/
+declare @xml xml;
+set @xml = (
+select
+        HumanResources.Employee.BusinessEntityID '@ID',
+        HumanResources.Employee.NationalIDNumber as 'NationalIDNumber',
+        HumanResources.Employee.JobTitle as 'JobTitle'
+    from
+        HumanResources.Employee
+    for xml
+        path ('Employee'),
+        root ('Employees')
+);
 
-create procedure dbo.sales(@years nvarchar(30)) as
-	declare @sql as nvarchar(700);
-	set @sql = '
-	select [Name] as Name ,
-		' + @years + '
-		from (
-		    select Production.Product.Name as Name, avz.OrderQty, OrderDate as OrderDate
-			from
-			(select Sales.SalesOrderDetail.ProductID as ProductID ,Sales.SalesOrderDetail.OrderQty, YEAR(Sales.SalesOrderHeader.OrderDate) as OrderDate
-			from Sales.SalesOrderDetail
-				 join Sales.SalesOrderHeader
-					on Sales.SalesOrderDetail.SalesOrderID = Sales.SalesOrderHeader.SalesOrderID) as avz
-					join Production.Product on avz.ProductID = Production.Product.ProductID
-			) as temp
-		PIVOT
-		(
-			SUM(temp.OrderQty)
-			FOR temp.OrderDate IN (' + @years + ')
-		) AS pvt'
-    exec sp_executesql @sql;
-go
-exec dbo.sales '[2008],[2007],[2006]';
-go
+create table dbo.tempEmployee
+(
+    BusinessEntityID int,
+	NationalIDNumber nvarchar(15),
+	JobTitle nvarchar(50)
+);
+
+insert into dbo.tempEmployee
+select
+   tempxml.Employee.value('@ID', 'int') as BusinessEntityID,
+   tempxml.Employee.query('NationalIDNumber').value('.', 'nvarchar(15)') as NationalIDNumber,
+   tempxml.Employee.query('JobTitle').value('.', 'nvarchar(50)') as JobTitle
+from @xml.nodes('Employees/Employee') as tempxml (Employee)
+
+select * from dbo.tempEmployee;
